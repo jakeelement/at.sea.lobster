49b351b929fa00d5609348a1996a64e4006c5658
jakeelement <74512300+jakeelement@users.noreply.github.com>
2026-05-19 11:05:35 -0300

    Fix MapBox georeferencing and remove attribution margins

--- R/map.data.R
+++ R/map.data.R
@@ -94,11 +94,33 @@ map.data <- function(data,
     map_width <- 900
     map_height <- 700
 
+    center_lon <- as.numeric(mean(xlim))
+    center_lat <- as.numeric(mean(ylim))
+    center_lat <- max(min(center_lat, 85), -85)
+
+    lon_range <- max(diff(xlim), 1e-06)
+
+    merc_y <- function(lat_deg) {
+      lat_rad <- lat_deg * pi / 180
+      log(tan(pi / 4 + lat_rad / 2))
+    }
+    inv_merc_y <- function(y) {
+      (2 * atan(exp(y)) - pi / 2) * 180 / pi
+    }
+
+    y_min <- merc_y(ylim[1])
+    y_max <- merc_y(ylim[2])
+    y_range <- max(y_max - y_min, 1e-09)
+
+    zoom_lon <- log2((map_width * 360) / (lon_range * 512))
+    zoom_lat <- log2((map_height * (2 * pi)) / (y_range * 512))
+    zoom_level <- max(0, min(22, floor(min(zoom_lon, zoom_lat))))
+
     mapbox_url <- paste0(
-      "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/[",
-      xlim[1], ",", ylim[1], ",", xlim[2], ",", ylim[2],
-      "]/", map_width, "x", map_height,
-      "?access_token=", mapbox_token
+      "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/",
+      center_lon, ",", center_lat, ",", zoom_level,
+      "/", map_width, "x", map_height,
+      "?logo=false&attribution=false&access_token=", mapbox_token
     )
 
     map_img_file <- tempfile(fileext = ".img")
@@ -114,9 +136,31 @@ map.data <- function(data,
         map_img <- magick::image_read(map_img_file)
         map_img <- as.raster(map_img)
 
-        plot(NA, xlim = xlim, ylim = ylim,
+        world_size <- 512 * (2 ^ zoom_level)
+        cx <- (center_lon + 180) / 360 * world_size
+        cy <- (1 - (log(tan(pi / 4 + (center_lat * pi / 180) / 2)) / pi)) / 2 * world_size
+
+        half_w <- map_width / 2
+        half_h <- map_height / 2
+
+        x_min_px <- cx - half_w
+        x_max_px <- cx + half_w
+        y_min_px <- cy - half_h
+        y_max_px <- cy + half_h
+
+        lon_min_img <- (x_min_px / world_size) * 360 - 180
+        lon_max_img <- (x_max_px / world_size) * 360 - 180
+
+        merc_y_top <- pi * (1 - 2 * (y_min_px / world_size))
+        merc_y_bot <- pi * (1 - 2 * (y_max_px / world_size))
+        lat_max_img <- inv_merc_y(merc_y_top)
+        lat_min_img <- inv_merc_y(merc_y_bot)
+
+        plot(NA,
+             xlim = c(lon_min_img, lon_max_img),
+             ylim = c(lat_min_img, lat_max_img),
              xlab = "Longitude", ylab = "Latitude", axes = TRUE, asp = 1)
-        rasterImage(map_img, xlim[1], ylim[1], xlim[2], ylim[2])
+        rasterImage(map_img, lon_min_img, lat_min_img, lon_max_img, lat_max_img)
         TRUE
       }, error = function(e) FALSE)
     }
